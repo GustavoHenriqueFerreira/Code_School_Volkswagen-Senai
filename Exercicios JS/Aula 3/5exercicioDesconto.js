8ce4ed4b20329377ea51e3c8b4761ff9febaa20f
Gustavo Henrique <82419986+GustavoHenriqueFerreira@users.noreply.github.com>
2023-07-14 00:24:25 -0300

    Exercicios JS - Aula 3

--- Exercicios JS/Aula 3/5exercicioDesconto.js
+++ Exercicios JS/Aula 3/5exercicioDesconto.js
@@ -3,7 +3,7 @@ let valorCompra = parseInt(prompt("Digite o valor da compra: "));
 let valorDescontado;
 
 if (valorCompra <= 100) {
-    console.log(`Você não possui desconto pois o valor da compra é R$100`);
+    console.log(`Você não possui desconto pois o valor da compra é menor que R$100`);
 } else if(valorCompra > 100 && valorCompra <= 200){
     valorDescontado = valorCompra * 0.9;
     console.log(`Você possui desconto de 10%, o valor da compra seria ${valorCompra} e agora será ${valorDescontado}`)
