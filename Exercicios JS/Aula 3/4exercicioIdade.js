6e323e28eb5b8e99ea0b3ac0bc966fe7a7d159a5
Gustavo Henrique <82419986+GustavoHenriqueFerreira@users.noreply.github.com>
2023-07-14 00:23:23 -0300

    Exercicios JS - Aula 3

--- Exercicios JS/Aula 3/4exercicioIdade.js
+++ Exercicios JS/Aula 3/4exercicioIdade.js
@@ -2,5 +2,7 @@ let prompt = require("prompt-sync")();
 let ano = parseInt(prompt("Digite seu ano de nascimento: "));
 let idade = new Date().getFullYear() - ano;
 if (idade >= 18) {
-    console.log(`Você é maior de idade e possui ${idade}`)
+    console.log(`Você é maior de idade e possui ${idade}`);
+} else {
+    console.log(`Você é menor de idade e possui ${idade}`);
 }
