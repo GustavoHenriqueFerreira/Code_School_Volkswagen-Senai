41f29b113abcfb5891c2a9ce63e36c14229fcbee
Gustavo-Henrique-Ferreira-Alves <gustavohferreiraalves@gmail.com>
2023-07-17 20:55:59 -0300

    feat: Exercicios JS - Aula 5

---
+++ Exercicios JS/Aula 5/exerciciosFor.js
@@ -0,0 +1,40 @@
+let prompt = require("prompt-sync")();
+
+/* Imprimir números de 1 a 10 utilizando o loop for */
+/* for (let i = 1; i <= 10; i++) {
+   console.log(i);
+} */
+
+/* Imprimir números pares de 1 a 10. */
+/* for (let i = 1; i <= 10; i++) {
+    if (i % 2 === 0){
+        console.log(i);
+    }
+} */
+
+/* Faça um programa que imprima os números ímpares de 0 a 50; */
+/* for (let i = 1; i <= 50; i++) {
+    if (i % 2 === 1){
+        console.log(i);
+    }
+} */
+
+/* Faça um programa que imprima na tela a tabuada de um número
+qualquer escolhido pelo usuário até o 10. */
+/* let numeroTabuada = parseInt(prompt("Digite um número para listar sua tabuada: "));
+for (let i = 1; i <= 10; i++) {
+    let valorTabuada = numeroTabuada * i;
+    console.log(valorTabuada); 
+} */
+
+/* Imprimir os primeiros 10 números da sequência de Fibonacci. */
+/* let numerosFibonnaci = []
+for (let i = 0; i < 10; i++) {
+    if (i === 0 || i === 1) {
+        numerosFibonnaci.push(1);
+    } else {
+        numerosFibonnaci.push(numerosFibonnaci[numerosFibonnaci.length - 1] + numerosFibonnaci[numerosFibonnaci.length - 2]);
+    }
+
+    console.log(numerosFibonnaci[i]);
+} */
